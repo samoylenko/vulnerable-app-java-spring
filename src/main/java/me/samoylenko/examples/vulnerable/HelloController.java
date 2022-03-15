9eb68cdf306267581a0ed4364320b4d8a25f591d
Michael Samoylenko <samoylenko@gmail.com>
2022-03-15 15:20:12 -0400

    Fix Cross Site Scripting (XSS) in '/hello'

--- src/main/java/me/samoylenko/examples/vulnerable/HelloController.java
+++ src/main/java/me/samoylenko/examples/vulnerable/HelloController.java
@@ -1,10 +1,11 @@
 package me.samoylenko.examples.vulnerable;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
     public String hello(@RequestParam String name) {
         return "Hello, " + name;
     }
